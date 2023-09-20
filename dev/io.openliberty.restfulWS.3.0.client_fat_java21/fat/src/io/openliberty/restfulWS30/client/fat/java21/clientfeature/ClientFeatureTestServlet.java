5fd9c0d0165b7f0a5ba5d5b9cef3aa5e3b0c0172
Chuck Bridgham <cbridgha@us.ibm.com>
2023-09-20 18:43:28 -0400

    Initial Fat Project holding VT compat tests

---
+++ dev/io.openliberty.restfulWS.3.0.client_fat_java21/fat/src/io/openliberty/restfulWS30/client/fat/java21/clientfeature/ClientFeatureTestServlet.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * Copyright (c) 2020 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.restfulWS30.client.fat.java21.clientfeature;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.Executors;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.Response;
+
+
+import org.junit.Test;
+
+import componenttest.app.FATServlet;
+
+@SuppressWarnings("serial")
+@WebServlet("/ClientFeatureTestServlet")
+public class ClientFeatureTestServlet extends FATServlet {
+
+    private static int PORT = Integer.getInteger("bvt.prop.HTTP_default", 8010);
+
+
+    @Test
+    public void testCanGETUsingOnlyClientFeature() throws Exception {
+        ClientBuilder builder = ClientBuilder.newBuilder();
+        Client client = builder.build();
+        try {
+            WebTarget target = client.target("http://localhost:" + PORT + "/clientfeature/endpoint");
+            Response response = target.request().header("x-name", "Robert").get();
+            assertEquals(200, response.getStatus());
+            assertEquals("Hello Robert", response.readEntity(String.class));
+        } finally {
+            client.close();
+        }
+    }
+    
+    @Test
+    public void testsetClientExecutor() throws Exception {
+        ClientBuilder builder = ClientBuilder.newBuilder();
+        //Test VT methods
+        builder.executorService(Executors.newVirtualThreadPerTaskExecutor());
+       
+        Client client = builder.build();
+        try {
+            WebTarget target = client.target("http://localhost:" + PORT + "/clientfeature/endpoint");
+            Response response = target.request().header("x-name", "Robert").get();
+            assertEquals(200, response.getStatus());
+            assertEquals("Hello Robert", response.readEntity(String.class));
+        } finally {
+            client.close();
+        }
+    }
+}
